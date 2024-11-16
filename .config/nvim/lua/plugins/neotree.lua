fb071b68d0bcdf04c93e7bc387adc82411775da7
Maxime Janvier <maxime.janvier@gmail.com>
2024-11-16 01:54:51 +0100

    hidden files

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -7,6 +7,19 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+ 	        hide_by_name = {},
+          never_show = { ".git" },
+	      }
+      }
+   })
+
     vim.keymap.set('n', '<C-n>', '<Cmd>:Neotree toggle<CR>')
   end
 }
