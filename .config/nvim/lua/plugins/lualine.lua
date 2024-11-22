24d56156c2d225de50b2dbce6338bf598f8e1bba
Maxime Janvier <maxime.janvier@gmail.com>
2024-11-22 02:53:01 +0100

    nvim

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,22 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'dracula'
-      }
-    })
-  end
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require('lualine').setup({
+        options = {
+          theme = 'dracula'
+        }
+      })
+    end
+  },
+  {
+    "akinsho/bufferline.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      vim.opt.termguicolors = true
+      require("bufferline").setup()
+    end
+  }
 }
--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -9,6 +9,11 @@ return {
   config = function()
     require("neo-tree").setup({
       filesystem = {
+        window = {
+          mappings = {
+            ["L"] = "open_nofocus",
+          },
+        },
         filtered_items = {
           visible = true,
           show_hidden_count = true,
@@ -16,7 +21,15 @@ return {
           hide_gitignored = false,
  	        hide_by_name = {},
           never_show = { ".git" },
-	      }
+	      },
+        commands = {
+          open_nofocus = function(state)
+            require("neo-tree.sources.filesystem.commands").open(state)
+            vim.schedule(function()
+              vim.cmd([[Neotree focus]])
+            end)
+          end,
+        },
       }
    })
 
